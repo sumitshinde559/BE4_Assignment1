be0f473da9333ebde1d0b2028e5e26b064ffbcc2
Your Name <sumitshinde559@gmail.com>
2026-05-05 18:24:20 +1000

    fix: PORT added to .env

--- index.js
+++ index.js
@@ -334,7 +334,7 @@ app.delete("/books/:bookId", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
